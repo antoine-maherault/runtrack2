8a86179bbb506acb574b3c5405ccbdee7d790bee
Antoine Mahérault <antoine.maherault@laplateforme.io>
2021-11-05 17:09:48 +0100

    Update form.php

--- jour04/job07/form.php
+++ jour04/job07/form.php
@@ -6,41 +6,27 @@
  <body>
 <?php 
 
-//html code// 
 
-//if h = 5 alors 5 traits murs et toît// 
-//if h = 10 alors 10 traits murs et toît// 
+// Toît 
 
-// if l = 10 alors 8 tirets bas 
-// if l = 20 alors 18 tirets bas
-
-
-
-$_GET["height"];
-$_GET["width"];
-
-
-// toît 
-
-    //TIP //    
-for($i = 0 ; $i<$_GET["width"];$i++){
- echo '&nbsp';
+    // TIP //    
+for($i = 1 ; $i<$_GET["width"];$i++){
+    echo '&nbsp';
 }
 echo '/\ <br/>';
-
     //__ //  
 
 $stabliser = $_GET["width"] / 2;
-$var_center = $_GET["width"]-(1.2*$_GET["height"]);
-$var_length =  $_GET["width"] / $_GET["height"]  ;
-$inc = $_GET["height"] - 1;
+$var_center = $_GET["width"]*0.9;
+$var_length =  $_GET["width"] / (1.1*$_GET["height"])  ;
+$inc = 0.003*$_GET["height"] + (0.1*$_GET["width"]);
    
-    //TUILES //   
+    // TOP //   
 
 
-    for($u = 1 ; $u<$_GET["height"];$u++){
-        // space //
-        if($var_length < $_GET["width"]){
+for($u = 1 ; $u<$_GET["height"];$u++){
+    // space //
+    if($var_length < $_GET["width"]){
         for($i = 0 ; $i<$var_center ;$i++){
             echo "&nbsp";
         }
@@ -54,30 +40,20 @@ $inc = $_GET["height"] - 1;
         // compteurs //
         $var_center = $var_center - $inc;
         $var_length =  $var_length  + $inc    ;
-
-        }
-        else{
-            echo "&nbsp";
-
-            echo '/';
-        for($a = 0; $a<$stabliser;$a++){
+    }
+    else{
+        echo '/';
+        for($o = 0 ; $o<$_GET["width"];$o++){
             echo "_";
         }
         echo '\ <br/>';    
-        }
     }
-
-
-
-  
-    
+    }
 
     //__ //    
    
   
-
-    //BASE //   
-
+    // BASE //   
 echo '/';
 for($i = 0 ; $i<$_GET["width"];$i++){
     echo "_";
@@ -86,7 +62,7 @@ echo '\ <br/>';
     //__ //    
 
 
-// murs 
+// Murs 
 
 for($i = 1 ; $i<$_GET["height"];$i++){
     echo '|';
@@ -95,7 +71,6 @@ for($i = 1 ; $i<$_GET["height"];$i++){
     }
     echo '|<br/>';
 }
-
 
 // sol  
 
