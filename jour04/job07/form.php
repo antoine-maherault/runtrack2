155d3feed538df48187df2a9813ca5a13b9ca043
Antoine Mahérault <antoine.maherault@laplateforme.io>
2021-11-06 19:11:10 +0100

    update job07

--- jour04/job07/form.php
+++ jour04/job07/form.php
@@ -10,46 +10,40 @@
 // Toît 
 
     // TIP //  
-$tip = $_GET["width"] / 20;
-for($i = $tip ; $i<$_GET["width"];$i++){
-    echo '&nbsp';
-}
-echo '/\ <br/>';
+// $tip = 2 ; // 2 FOR 10x5
+// for($i = $tip ; $i<($_GET["width"]);$i++){
+//     echo '&nbsp';
+// }
+// echo '/\ <br/>';
 
     //__ //  
 
-$stabliser = $_GET["width"] / 2;
-$var_center = $_GET["width"]*0.9;
-$var_length =  $_GET["width"] / (1.1*$_GET["height"])  ;
-$inc = 0.003*$_GET["height"] + (0.1*$_GET["width"]);
-   
+$stabliser = $_GET["width"] / 2; // 2 FOR 10x5
+$var_center = $_GET["width"]-2; // -4 FOR 10x5 -6 _ FOR 20x5 _ -8 FOR 30x5 _ -10 FOR 40x5
+$var_length =  $_GET["width"] -2 ; // -5 FOR 10x5 _ -10 FOR 20*10 _-20 FOR 40$20
+$inc = $_GET["width"] / $_GET["height"]; // 2 FOR 10x5 _ 6 FOR 20x5 _ 9 FOR 30x5
+$length = 0;
     // TOP //   
 
 
-for($u = 1 ; $u<$_GET["height"];$u++){
+for($u = 1 ; $u<$_GET["height"];$u++){   
+    
     // space //
-    if($var_length < $_GET["width"]){
         for($i = 0 ; $i<$var_center ;$i++){
             echo "&nbsp";
         }
         // length_étage //
         echo '/';
-        for($a = 0; $a<$var_length;$a++){
+        for($a = 0; $a<$length;$a++){
             echo "_";
         }
         echo '\ <br/>';    
 
         // compteurs //
         $var_center = $var_center - $inc;
-        $var_length =  $var_length  + $inc    ;
-    }
-    else{
-        echo '/';
-        for($o = 0 ; $o<$_GET["width"];$o++){
-            echo "_";
-        }
-        echo '\ <br/>';    
-    }
+        $var_length =  $var_length  + $inc;
+        $length = $length + $inc;
+  
     }
 
     //__ //    
@@ -57,7 +51,7 @@ for($u = 1 ; $u<$_GET["height"];$u++){
   
     // BASE //   
 echo '/';
-for($i = 0 ; $i<$_GET["width"];$i++){
+for($i = 2 ; $i<$_GET["width"];$i++){
     echo "_";
 }
 echo '\ <br/>';
@@ -68,7 +62,7 @@ echo '\ <br/>';
 
 for($i = 1 ; $i<$_GET["height"];$i++){
     echo '|';
-    for($u = 0 ; $u<$_GET["width"];$u++){
+    for($u = 2 ; $u<$_GET["width"];$u++){
         echo "&nbsp&nbsp";
     }
     echo '|<br/>';
@@ -77,14 +71,13 @@ for($i = 1 ; $i<$_GET["height"];$i++){
 // sol  
 
 echo '|';
-for($i = 0 ; $i<$_GET["width"];$i++){
+for($i = 2 ; $i<$_GET["width"];$i++){
     echo "_";
 }
 echo '|';
 
 
 ?>
-
 
 
 
