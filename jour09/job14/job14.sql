47e7827b31b5d559704b826e021f9fb2f006e5c1
Antoine Mahérault <antoine.maherault@laplateforme.io>
2021-11-16 13:40:04 +0100

    update

--- jour09/job14/job14.sql
+++
@@ -1,4 +0,0 @@
-
--- Selectionner ensemble des champs de la table etudiants
-
-SELECT prenom, nom, naissance FROM `etudiants` WHERE naissance BETWEEN '1998-01-01' AND '2018-01-01'
--- jour09/job18/job18.sql
+++
@@ -1,4 +0,0 @@
-
--- Selectionner ensemble des champs de la table etudiants
-
-SELECT nom AS BiggestRoom, id_etage, capacite FROM `salles` HAVING capacite = (SELECT MAX(capacite) FROM `salles`)
